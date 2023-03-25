b0d12e5d7ded7f76a9d2e77155ef7639bcb52a44
kramargit <kramar.web@gmail.com>
2023-03-25 16:28:49 +0700

    создано mvp для api

--- api/src/index.js
+++ api/src/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const {ApolloServer, gql} = require('apollo-server-express');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+const cors = require('cors');
+const depthLimit = require('graphql-depth-limit');
+const { createComplexityLimitRule } = require('graphql-validation-complexity');
 
 const db = require('./db');
 const models = require('./models');
@@ -24,12 +28,16 @@ const DB_HOST = process.env.DB_HOST;
 
 const app = express();
 
+app.use(helmet());
+app.use(cors());
+
 db.connect(DB_HOST);
 
 // Настраиваем Apollo Server
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
     context: ({ req }) => {
         // получаем токен пользователя из заголовков
         const token = req.headers.authorization;
